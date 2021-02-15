011a04f56aec8c0d436d0538ea3642e4d6a7a943
pey1123 <79104846+pey1123@users.noreply.github.com>
2021-02-16 01:05:52 +0800

    Add files via upload

---
+++ 简单c（test）.c
@@ -0,0 +1,17 @@
+//包含一个叫stdio.h的文件
+#include<stdio.h>
+int main()//主函数-程序的入口-有且仅有一个
+{
+		//这里完成任务
+		//输出hello world 
+		//函数-print function
+		//库函数-c语言本身提供给我们使用的函数
+		//别人的东西-打招呼
+		//#include
+	printf("hello world\n");
+	return 0;
+} 
+
+//int 为整型的意思
+//main 前面的 int 表示main 函数调用返回一个整型值
+//或 void main
